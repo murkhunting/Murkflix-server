bddb180b8f4c2643c62d4426e5a21a1783ffb8de
murkhunting <marcmp.arq@gmail.com>
2021-12-10 12:04:36 +0100

    sign up, encrypt password & postman comprobation

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //SIGN UP
 router.post("/signup", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
   });
 
   try {
-    const user = newUser.save();
+    const user = await newUser.save();
     res.json(user);
   } catch (err) {
     res.status(500).json(err);
