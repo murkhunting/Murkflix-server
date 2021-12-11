d405650ddf1896903640efd93480e5f8017441f4
murkhunting <marcmp.arq@gmail.com>
2021-12-11 21:40:42 +0100

    user get and get all

--- routes/users.js
+++ routes/users.js
@@ -44,6 +44,32 @@ router.delete("/:id", verify, async (req, res) => {
   }
 });
 
+//GET
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET ALL
+router.get("/", verify, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query ? await User.find().limit(10) : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
+  }
+});
+
 //GET USER STATS
 
 module.exports = router;
