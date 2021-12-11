49a0ca1e8592e575caafaf6b29f6a66b509c510e
murkhunting <marcmp.arq@gmail.com>
2021-12-11 21:39:29 +0100

    delete user

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
+const verify = require("../verifyToken");
 
 //UPDATE
-
-router.put("/:id", async (req, res) => {
+router.put("/:id", verify, async (req, res) => {
   if (req.user.id === req.params.id || req.user.isAdmin) {
     if (req.body.password) {
       req.body.password = CryptoJS.AES.encrypt(
@@ -14,10 +14,14 @@ router.put("/:id", async (req, res) => {
     }
 
     try {
-      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
-      res.params.status(200).json(updatedUser);
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
     }
@@ -25,7 +29,21 @@ router.put("/:id", async (req, res) => {
     res.status(403).json("You can update only your account");
   }
 });
+
 //DELETE
-//GET
-//GET ALL
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted!");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can delete only your account");
+  }
+});
+
 //GET USER STATS
+
+module.exports = router;
