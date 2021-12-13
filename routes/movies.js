dc41f35f05110b34e24cb2f62b486597b053e590
murkhunting <marcmp.arq@gmail.com>
2021-12-13 11:49:04 +0100

    get all movies

--- routes/movies.js
+++ routes/movies.js
@@ -52,7 +52,7 @@ router.delete("/:id", verify, async (req, res) => {
 });
 
 //GET
-router.get("/:id", verify, async (req, res) => {
+router.get("/find/:id", verify, async (req, res) => {
   try {
     const movie = await Movie.findById(req.params.id);
     res.status(200).json(movie);
@@ -77,8 +77,23 @@ router.get("/random", verify, async (req, res) => {
         { $sample: { size: 1 } },
       ]);
     }
+    res.status(200).json(movie);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//GET ALL
+router.get("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const movies = await Movie.find();
+      res.status(200).json(movies.reverse());
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed!");
   }
 });
 
