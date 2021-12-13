4b321a0fddaa791222eb6b984ebb194e4ba693a7
murkhunting <marcmp.arq@gmail.com>
2021-12-13 09:29:59 +0100

    delete movie

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ const Movie = require("../models/Movie");
 const verify = require("../verifyToken");
 
 //CREATE
-router.put("/", verify, async (req, res) => {
+router.post("/", verify, async (req, res) => {
   if (req.user.isAdmin) {
     const newMovie = new Movie(req.body);
     try {
@@ -29,6 +29,20 @@ router.put("/:id", verify, async (req, res) => {
         { new: true }
       );
       res.status(200).json(updatedMovie);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+
+//DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie has been deleted");
     } catch (err) {
       res.status(500).json(err);
     }
