d40fc8d147942507e3838cd41fc753eb56fb6d2c
murkhunting <marcmp.arq@gmail.com>
2021-12-13 13:22:56 +0100

    delete list

--- routes/lists.js
+++ routes/lists.js
@@ -18,6 +18,18 @@ router.post("/", verify, async (req, res) => {
 });
 
 //DELETE
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await List.findByIdAndDelete(req.params.id);
+      res.status(200).json("The list has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
 
 //GET
 module.exports = router;
